33ba7269b7000cb71995dac4cc906441c428fbf8
Matthias Ervens <matthias.ervens@smail.fh-koeln.de>
2015-10-17 22:28:16 +0200

    added CreateShareActivity

---
+++ src/org/tuxship/quickshare/CreateShareActivity.java
@@ -0,0 +1,65 @@
+package org.tuxship.quickshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateShareActivity extends Activity {
+
+	Button submitBtn;
+	EditText shareNameInput; 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_share);
+		
+		setup();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.create_share, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private void setup() {
+		submitBtn = (Button)findViewById(R.id.submitBtn);
+		shareNameInput = (EditText)findViewById(R.id.shareNameInput);s
+		
+		submitBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String input = shareNameInput.getText().toString();
+				
+				if(input.equals(""))
+					return;
+				
+				TokenDatabase dao = TokenDatabase.getInstance();
+				
+				/*
+				 * TODO Save in database
+				 */
+				
+			}
+		});
+	}
+}

b949b273b9ed03a2e906b71d5becfd1a08a97a17
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2015-10-17 23:00:57 +0200

    bannaa

--- gen/org/tuxship/quickshare/R.java
+++ gen/org/tuxship/quickshare/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080005;
         public static final int captionText=0x7f080000;
-        public static final int hello_world=0x7f080003;
+        public static final int filelist=0x7f080004;
+        public static final int remote_link=0x7f080003;
         public static final int shareNameInput=0x7f080001;
         public static final int submitBtn=0x7f080002;
     }

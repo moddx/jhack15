3e0928ed7e47a6eebcbf4023840134c6ee656a6e
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2015-10-18 09:58:04 +0200

    webserver shows filelist

--- src/org/tuxship/quickshare/Httpd.java
+++ src/org/tuxship/quickshare/Httpd.java
@@ -1,16 +1,20 @@
 package org.tuxship.quickshare;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.tuxship.quickshare.TokenDatabase.LocalBinder;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.util.Log;
 import fi.iki.elonen.NanoHTTPD;
@@ -19,6 +23,9 @@ import fi.iki.elonen.NanoHTTPD;
 public class Httpd extends Service
 {
     private WebServer server;
+    
+    TokenDatabase dbService;
+    boolean dbBound = false;
     
     public Httpd() {
     	
@@ -97,6 +104,14 @@ public class Httpd extends Service
 	            		+ "<p>Your Token: <input type='text' name='accessToken'><input type='submit' value='submit'></p>\n" + "</form>\n";
 	        } else {
 	            page += "<p>Hello, here are your files (Token: " + parms.get("accessToken") + " )</p>";
+	            page += "<table><tr><th>Your Files:</th></tr>\n";
+	            if(dbBound){
+	            	List<String> files=dbService.getFilesforToken(parms.get("accessToken"));
+	            	for(int i =0;i<files.size();i++){
+	            		page +="<tr><td>"+files.get(i)+"</td></tr>\n";
+	            	}
+	            }
+	            page += "</table>\n";
 	        }
 	        
 	        page += "</div>";
@@ -111,4 +126,20 @@ public class Httpd extends Service
 	    }
 	}
     
+	/** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // We've bound to the TokenDatabase, cast the IBinder and get TokenDatabase instance
+            LocalBinder binder = (LocalBinder) service;
+            dbService = binder.getService();
+            dbBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            dbBound = false;
+        }
+    };
 }
